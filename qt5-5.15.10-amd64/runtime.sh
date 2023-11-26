e98ed0b2517c3b6488ee967d7e8bb24acfb77d3d
ziggy <ziggy16603042386@gmail.com>
2023-11-26 16:34:01 +0800

    Add Qt5.15.10 OpenGLES envs

--- qt5-5.15.10-amd64/runtime.sh
+++ qt5-5.15.10-amd64/runtime.sh
@@ -1,2 +1,4 @@
 export PREPEND_ENV__LD_LIBRARY_PATH=(":" "%ROOT%/lib")
 export PREPEND_ENV__QT_PLUGIN_PATH=(":" "%ROOT%/plugins")
+export PREPEND_ENV__QT_QPA_PLATFORM_PLUGIN_PATH=(":" "%ROOT%/plugins/platforms")
+export PREPEND_ENV__QML2_IMPORT_PATH=(":" "%ROOT%/qml")
